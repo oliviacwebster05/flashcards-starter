ed0fc6f163997d8afeb18bdf164d950150bc9431
Olivia Webster <oliviacweb@gmail.com>
2020-03-19 19:35:54 -0600

    Refactor tests to es6

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,20 +6,20 @@ const Card = require('../src/Card');
 
 describe('Turn', () => {
   let card, turn;
-  beforeEach(function() {
+  beforeEach(() => {
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     turn = new Turn("array", card);
   });
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Turn).to.be.a('function');
   });
   it('should be an instance of Turn', () => {
     expect(turn).to.be.an.instanceof(Turn);
   });
-  it('Turn should be instantiated with a card', function() {
+  it('Turn should be instantiated with a card', () => {
     expect(turn.card).to.equal(card);
   });
-  it('Turn should be instantiated with a guess', function() {
+  it('Turn should be instantiated with a guess', () => {
     expect(turn.guess).to.equal("array");
   });
 })
