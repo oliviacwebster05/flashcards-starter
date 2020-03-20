11a3bbecda67c6c5b2efdf93e75500d09c154b50
Olivia Webster <oliviacweb@gmail.com>
2020-03-20 03:39:12 -0600

    Add tests and start method to game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,15 @@
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+    this.cards = [];
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +18,17 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+  start() {
+      prototypeQuestions.forEach(question => {
+        let card = new Card(question.id, question.question, question.answers, question.correctAnswer);
+        this.cards.push(card);
+      })
+    const deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck);
+    this.printQuestion(round);
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,20 +1,37 @@
 const chai = require('chai');
 const expect = chai.expect;
-const Game = require('../src/Game');
+const data = require('../src/data');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Card = require('../src/Card');
+const Game = require('../src/Game');
 
 describe('Game', () => {
-  let game;
+  let game, deck, round;
+
   beforeEach(() => {
     game = new Game();
+    card1 = new Card(data.prototypeData[0]);
+    card2 = new Card(data.prototypeData[1]);
+    card3 = new Card(data.prototypeData[2]);
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck)
   });
-  it('should be a function', function() {
+
+  it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
-  it('should be an instance of Game', function() {
-    const game = new Game();
+
+  it('should be an instance of Game', () => {
     expect(game).to.be.an.instanceof(Game);
   });
-})
+
+  it('should create a new instance of Deck when game starts', () => {
+    game.start()
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+  it('should create a new instance of Round when ga,e starts', () => {
+    game.start()
+    expect(round).to.be.an.instanceof(Round);
+  });
+});
