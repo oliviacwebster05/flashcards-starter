f8d09693b966de8feb76bd92ea40c34c4678815d
Olivia Webster <oliviacweb@gmail.com>
2020-03-19 19:34:19 -0600

    Refactor Turn test to include beforeEach

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,22 +4,22 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 
-describe('Turn', function() {
+describe('Turn', () => {
+  let card, turn;
+  beforeEach(function() {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn("array", card);
+  });
   it('should be a function', function() {
     expect(Turn).to.be.a('function');
   });
-  it('should be an instance of Turn', function() {
-    const turn = new Turn();
+  it('should be an instance of Turn', () => {
     expect(turn).to.be.an.instanceof(Turn);
   });
   it('Turn should be instantiated with a card', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn("array", card);
     expect(turn.card).to.equal(card);
   });
   it('Turn should be instantiated with a guess', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn("array", card);
     expect(turn.guess).to.equal("array");
   });
 })
