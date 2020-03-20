b61416181a2c76f5119783c91958405b825490df
Olivia Webster <oliviacweb@gmail.com>
2020-03-19 20:17:40 -0600

    Add test structure and first test for Deck

---
+++ test/Deck-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', () => {
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function');
+  });
+});
